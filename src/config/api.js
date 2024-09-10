46a154aaadd207f4e14278325c30957418cb708f
HARSHVARDHAN PUROHIT <75427196+Harshvardhan1609@users.noreply.github.com>
2024-09-11 02:12:01 +0530

    final

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "1cc54de21f4913b79e138ffe517acb20";
+const API_KEY = process.env.REACT_APP_API_KEY;
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
